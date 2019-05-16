4759fa8302862b374ae99bfe58428a5b5f9bb8d1
Joe Dinius <joe.dinius@ubtrobot.com>
2019-05-16 10:49:37 -0700

    adding factor analyzer

--- Ch7/utilities.py
+++ Ch7/utilities.py
@@ -117,8 +117,9 @@ def fit_student_distribution(training_data, stopping_thresh=1e-2):
 	nu = 1000.
 	
 	# define objective function for maximizing likelihood wrt nu
-	def t_fit_cost(nu, _E_h, _E_logh):
-		return -(float(_E_h.size) * (0.5 * nu * np.log(0.5 * nu) + sci_spec.gammaln(0.5 * nu)) - (0.5 * nu - 1.) * np.sum(_E_logh) + 0.5 * nu * np.sum(_E_h))
+	def t_fit_cost(nu, sum_E_h, sum_E_log_h, I):
+		nu_over_2 = 0.5 * nu
+		return -(float(I) * (nu_over_2 * np.log(nu_over_2) + sci_spec.gammaln(nu_over_2)) - (nu_over_2 - 1.) * sum_E_log_h + nu_over_2 * sum_E_h)
 
 	# initialize log-likelihood
 	L_prev = None
@@ -126,11 +127,11 @@ def fit_student_distribution(training_data, stopping_thresh=1e-2):
 	while True:
 		its += 1
 		# Expectation step
-		x_minus_mu = np.array([d - mu for d in training_data])
-		inv_sigma = np.linalg.inv(Sigma)
+		x_minus_mu = np.array([np.copy(d - mu) for d in training_data])
+		inv_sigma = np.linalg.inv(np.copy(Sigma))
 		delta = np.array([d.reshape((1, int(D)))@inv_sigma@d.reshape((int(D), 1)) for d in x_minus_mu]).flatten()
 		E_h = np.array([(nu + D) / (nu + d) for d in delta]).flatten()
-		E_logh = np.array([sci_spec.digamma((nu+D)/2.) - np.log((nu+d)/2.) for d in delta]).flatten()
+		E_logh = np.array([sci_spec.digamma(0.5*(nu+D)) - np.log(0.5*(nu+d)) for d in delta]).flatten()
 		
 		# Maximization step
 		sum_E_h = np.sum(E_h)
@@ -138,20 +139,19 @@ def fit_student_distribution(training_data, stopping_thresh=1e-2):
 		for i in range(int(I)):
 			sum_E_h_times_x += E_h[i] * training_data[i]
 		mu = sum_E_h_times_x / sum_E_h
-		x_minus_mu = np.array([d - mu for d in training_data])
+		x_minus_mu = np.array([np.copy(d - mu) for d in training_data])
 		Sigma = np.zeros((int(D), int(D)))
 		for i in range(int(I)):
 			d = x_minus_mu[i]
 			Sigma += E_h[i] * np.outer(d, d) / sum_E_h
-		inv_sigma = np.linalg.inv(Sigma)
+		inv_sigma = np.linalg.inv(np.copy(Sigma))
 		delta = np.array([d.reshape((1, int(D)))@inv_sigma@d.reshape((int(D), 1)) for d in x_minus_mu]).flatten()
-		x0 = nu
 		bnds = [(1e-15, 1e3)]
-		sol = minimize(t_fit_cost, x0, args=(E_h, E_logh), bounds=bnds)
-		nu = sol.x[0]
+		sol = minimize(t_fit_cost, nu, args=(np.sum(E_h), np.sum(E_logh), I), bounds=bnds)
+		nu = np.copy(sol.x)[0]
 		
 		# Compute the log likelihood
-		L = I * (sci_spec.gammaln((nu + D)/2.) - (D/2.) * np.log(nu * np.pi)- np.log(np.linalg.det(Sigma)) / 2. - sci_spec.gammaln(nu/2.))
+		L = I * (sci_spec.gammaln(0.5*(nu + D)) - 0.5 * D * np.log(nu * np.pi) - 0.5 * np.log(np.linalg.det(Sigma)) - sci_spec.gammaln(0.5 * nu))
 		s = np.sum([np.log(1. + d/nu) for d in delta])
 		L -= 0.5*(nu + D) * s
 		if L_prev is None:
@@ -163,6 +163,67 @@ def fit_student_distribution(training_data, stopping_thresh=1e-2):
 		L_prev = L
 	return mu, Sigma, nu
 
+
+def fit_factor_analyzer(training_data, num_factors, stopping_thresh=1e-2):
+	# Algorithm 7.3
+	I, D = [float(t) for t in training_data.shape]
+	
+	# initialization: follows footnote a) of algorithm guide
+	rng = np.random.RandomState(seed=0)
+	mu = np.mean(training_data, axis=0)
+	x_minus_mu = np.array([d - mu for d in training_data])
+	Sigma = np.zeros((int(D), int(D)))
+	for d in x_minus_mu:
+		Sigma += np.diag(np.diag(np.outer(d, d))) / I
+	# DxK
+	Phi = rng.random((int(D), int(num_factors)))
+	x_minus_mu = np.array([d - mu for d in training_data])
+
+	its = 0
+	while True:
+		its += 1
+		# Expectation step
+		inv_sigma = np.linalg.inv(Sigma)
+		# KxD
+		M = Phi.T@np.linalg.inv(Sigma)
+		# KxK
+		_I = np.eye(int(D))
+		# KxD
+		_M = np.linalg.inv(M@Phi + I)
+		E_h = np.zeros((int(I), int(num_clusters)))
+		E_hht = np.zeros((int(I), int(num_clusters), int(num_clusters)))
+		for i in range(int(I)):
+			E_h[i, :] = ((_M@M)@x_minus_mu[i].T).T
+			E_hht[i, :, :] = _M + E_h[i, :].T@E_h[i, :]
+
+		# Maximization step
+		sum_E_hht = np.zeros((int(num_clusters), int(num_clusters)))
+		Phi = np.zeros((int(D), int(num_factors)))
+		Sigma = np.zeros((int(D), int(D)))
+		for i in range(int(I)):
+			d = x_minus_mu[i].T
+			sum_E_hht += E_hht[i, :, :].reshape((int(num_clusters), int(num_clusters)))
+			Phi += d@E_h[i]
+			ddt = d@d.T
+			Sigma += ddt - Phi@E_h[i, :].T@d.T
+		Phi = Phi@np.linalg.inv(sum_E_hht)
+		Sigma = np.diag(np.diag(Sigma)) / I
+
+		# Compute Data Log Likelihood and EM Bound
+		tmp = np.zeros((int(I),)).flatten()
+		for i in range(int(I)):
+			for k in range(num_clusters):
+				_Sigma = Sigma + Phi@Phi.T
+				tmp[i] = stats.multivariate_normal.pdf(training_data[i, :], mean=mu, cov=_Sigma)
+		L = np.sum(np.log(tmp))
+		if L_prev is None:
+			L_prev = L
+			continue
+		if np.abs(L-L_prev) < stopping_thresh:
+			print('stopping criteria met after {its} iterations.'.format(its=its))
+			break
+		L_prev = L
+	return mu, Phi, Sigma
 
 class TestFMOG(unittest.TestCase):
 	def test_basic_call(self):
@@ -176,17 +237,24 @@ class TestFMOG(unittest.TestCase):
 		_lambdas, mus, Sigmas = fit_gaussian_mixture(X, 2)
 
 class TestStudentFit(unittest.TestCase):
-	# just check to make sure the call is functioning for nominal input
-	rng = np.random.RandomState(seed=0)
-	mu1 = np.array([1., 2.])
-	cov1 = np.array([[2., 0.], [0., .5]])
-	num_data_pts = np.array([200])
-	X = rng.multivariate_normal(mu1, cov1, num_data_pts[0])
-	num_outliers = 15
-	outliers_mu = np.array([5., 7.])
-	outliers_Sigma = np.array([[.2, 0.], [0., .2]])
-	X_with_outliers = np.vstack((X, rng.multivariate_normal(outliers_mu, outliers_Sigma, num_outliers)))
-	t_mu, t_sig, t_nu = fit_student_distribution(X_with_outliers, stopping_thresh=1e-6)
+	def test_basic_call(self):
+		# just check to make sure the call is functioning for nominal input
+		rng = np.random.RandomState(seed=0)
+		mu1 = np.array([1., 2.])
+		cov1 = np.array([[2., 0.], [0., .5]])
+		num_data_pts = np.array([200])
+		X = rng.multivariate_normal(mu1, cov1, num_data_pts[0])
+		num_outliers = 15
+		outliers_mu = np.array([5., 7.])
+		outliers_Sigma = np.array([[.2, 0.], [0., .2]])
+		X_with_outliers = np.vstack((X, rng.multivariate_normal(outliers_mu, outliers_Sigma, num_outliers)))
+		t_mu, t_sig, t_nu = fit_student_distribution(X_with_outliers, stopping_thresh=1e-6)
+
+class TestFactorAnalyzerFit(unittest.TestCase):
+	def test_basic_call(self):
+		# TODO(jwd) - fill this in!
+		pass
+
 
 if __name__ == '__main__':
 	unittest.main()
